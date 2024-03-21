e51a7e660b2079081597cde96482f590d372058d
Adrien Yakite <adrien.yakite@gmail.com>
2024-03-21 14:34:30 +0100

    fix affichage "voir photo" style du form modifié gestion du form prise en charge modifs de style

--- files.js
+++ files.js
@@ -1,14 +1,15 @@
 //gestion du form de contact
 let sectionForm = document.querySelector(".formContact");
-let btnEnvoi = document.querySelector("#btnEnvoi");
+let formulaireContact= document.getElementById("formContact");
 let inputNom = document.querySelector("#nom");
 
-btnEnvoi.addEventListener("submit", (event)=>{
+formulaireContact.addEventListener("submit", (event)=>{
     event.preventDefault();
 
     console.log("bouton cliqué !");
 
     let nomUser = inputNom.value;
     sectionForm.innerHTML=``;
-    sectionForm.innerHTML=`Merci pour votre message + ${nomUser} ! <br> Je vous recontacterai au plus vite:)`;
+    sectionForm.innerHTML=`Merci pour votre message ${nomUser} ! Je vous recontacterai au plus vite:)`;
+    sectionForm.classList.add("classeTemporaireForm")
 })
